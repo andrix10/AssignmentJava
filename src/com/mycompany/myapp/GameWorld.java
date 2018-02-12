565d8c6e6819539af7eab433d1cc6f42c32118a4
andrix10 <ender2013@gmail.com>
2018-02-11 23:21:13 -0800

    removed n

--- src/com/mycompany/myapp/GameWorld.java
+++ src/com/mycompany/myapp/GameWorld.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameWorld {
-	private ArrayList<GameObject> objlist = n;
+	private ArrayList<GameObject> objlist;
 	
 	public void init() {
 		
